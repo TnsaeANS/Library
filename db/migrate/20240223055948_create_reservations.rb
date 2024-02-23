7d9cf63c48408a944e4b4b849879b19f80dbb4f1
Tnsae <tnsae.alemayehu@bitscollege.edu.et>
2024-02-23 18:56:01 +0300

    rspec(passed), validations, controllers

--- db/migrate/20240223055948_create_reservations.rb
+++ db/migrate/20240223055948_create_reservations.rb
@@ -1,8 +1,7 @@
-class CreateLends < ActiveRecord::Migration[7.1]
+class CreateReservations < ActiveRecord::Migration[7.1]
   def change
-    create_table :lends do |t|
-      t.date :lent_date, null: false
-      t.date :return_date, null: false
+    create_table :reservations do |t|
+      t.date :reservation_date, null: false
       t.references :user, null: false, foreign_key: true
       t.references :book, null: false, foreign_key: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_22_095306) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_23_055948) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_22_095306) do
     t.index ["user_id"], name: "index_lends_on_user_id"
   end
 
+  create_table "reservations", force: :cascade do |t|
+    t.date "reservation_date"
+    t.bigint "user_id", null: false
+    t.bigint "book_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_reservations_on_book_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
@@ -47,4 +57,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_22_095306) do
 
   add_foreign_key "lends", "books"
   add_foreign_key "lends", "users"
+  add_foreign_key "reservations", "books"
+  add_foreign_key "reservations", "users"
 end
