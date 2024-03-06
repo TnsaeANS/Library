7e6f76b86884578e8b0f4543a0e2bf018e5707fc
Tnsae <tnsae.alemayehu@bitscollege.edu.et>
2024-03-06 15:41:52 +0300

    linked the buttons

--- lms-client/src/Components/user.jsx
+++ lms-client/src/Components/user.jsx
@@ -16,8 +16,11 @@ const User = () => {
                     <div className='l1'>
                         <button className='request_book-button' >Request Book</button>
                         <button className='reserve_book-button' >Reserve Book</button>
-                        <button className='recently_added-button' >Recently Added</button>
-                    </div>
+                        <div className="action">
+ <Link to="/recently">
+    <button className="recently_added-button">Recently Added</button>
+ </Link>
+</div>                    </div>
                 </div>
             </div>
         </>
